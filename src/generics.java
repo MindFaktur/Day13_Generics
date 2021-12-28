aebdbc3b5f89311539de483a15009a7920111ed4
Vishwanath <vishwanathiti828@gmail.com>
2021-12-28 12:55:24 +0530

    added integer max finder

--- src/generics.java
+++ src/generics.java
@@ -1,5 +1,23 @@
 public class generics {
-    public static void main(String[] args) {
-        System.out.println("Welcome to generics program");
+
+    public static Integer findmax(Integer int_1, Integer int_2, Integer int_3) {
+        Integer max;
+        if( int_1.compareTo(int_2) >= 0 ){
+            max = int_1;
+        }else if ( int_2.compareTo(int_3) >= 0 ){
+            max = int_2;
+        }else{
+            max = int_3;
+        }
+        return max;
     }
+
+    public static void main(String[] args) {
+        System.out.println(findmax(10,25,30));
+        System.out.println(findmax(10,55,30));
+        System.out.println(findmax(99,25,30));
+    }
+
+
 }
+
